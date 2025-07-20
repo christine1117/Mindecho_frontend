a0eb5dd4d5325638321f7c1455d06a900a2e45fc
christine <christine921117@gmail.com>
2025-07-20 21:46:11 +0800

    Initial Commit

---
+++ mindecho/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mindecho
+//
+//  Created by 鄧巧婕 on 2025/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mindecho/mindechoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindechoApp.swift
+//  mindecho
+//
+//  Created by 鄧巧婕 on 2025/7/20.
+//
+
+import SwiftUI
+
+@main
+struct mindechoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
